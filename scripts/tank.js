352fd2c846ddf32e5bd93921e50e4af62e2cd868
jian <acui145@gmail.com>
2016-03-10 01:52:01 +0800

    finish crazy kill mode

--- scripts/tank.js
+++ scripts/tank.js
@@ -14,7 +14,8 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var elemUI = document.getElementById('ui');
 var elemTank = document.getElementById('tank');
-
+var elemMisc = document.getElementById('misc');
+var elemBoom = document.getElementById('boom');
 var game = {
     //canvas的大小（和html中的canvas保持一致）
     w: 538,
@@ -26,6 +27,11 @@ var tankSize = {
     w: 32,
     h: 32
 };
+var bombSize = {
+    w: 64,
+    h: 64
+}
+var dirList = ["top", "bottom", "left", "right"];
 var tankPicsNPC = {
     'top': [9, 27],
     'bottom': [81, 99],
@@ -44,6 +50,13 @@ var tankPicsP2 = {
     'left': [41, 59],
     'right': [113, 131]
 };
+var bulletPics = {
+    'top': [0, 0],
+    'right': [8, 0],
+    'bottom': [16, 0],
+    'left': [24, 0]
+};
+
 var calPic = function(num) {
     //根据坦克所在位置来计算像素开始的位置
     return (num - 1) * 32;
@@ -54,6 +67,7 @@ var gameMode = {
 };
 var boolTankAni = false; //让选择画面的坦克动起来
 var tankList = [];
+var bulletList = [];
 /**
  * 渲染画布，不同阶段对应不同画面
  * @param {Num} status {0,1,2}
@@ -112,10 +126,18 @@ function drawCanvas(status, y) {
             break;
         case 3:
             //进入游戏画面（打开幕布）
-            crazyKill(mode0Stage);
+            if (game.mode === 0) crazyKill(mode0Stage);
+            if (game.mode === 1) civilWar(mode1Stage);
+
     }
 }
+var mode1Stage = 0;
+var civilWar = function(stage) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, game.w, game.h);
+}
 var mode0Stage = 0;
+var mode0StageStop = 3;
 var crazyKill = function(stage) {
     if (stage === 0) {
         if (pageCurtain.x < game.w / 2) {
@@ -146,16 +168,44 @@ var crazyKill = function(stage) {
         mode0Stage = 2;
     }
     if (stage === 2) {
-        // tankList[0].move();
+        //随机出现新NPC
+        createNewNPC(20);
+        killBullet();
+        //检查子弹，并且让子弹移动
+        for (var i = bulletList.length - 1; i >= 0; i--) {
+            bulletList[i].fly();
+            //如果子弹超出范围，从列表中消除
+            if (bulletList[i].curX < 0 || bulletList[i].curX > game.w || bulletList[i].curY < 0 || bulletList[i].curY > game.h) {
+                ctx.clearRect(bulletList[i].curX, bulletList[i].curY, bulletSize.w, bulletSize.h);
+                bulletList.splice(i, 1);
+            }
+        }
+        //检查子弹是否撞上坦克
+        killTank();
+        for (var i = bombList.length - 1; i >= 0; i--) {
+            bombList[i].showExplode();
+            if (bombList[i].times < 0) {
+                bombList.splice(i, 1);
+            }
+        }
         for (var i = 0; i < tankList.length; i++) {
-            var tempTank = tankList[i];
-            if (tempTank.type === "NPC") {
-                npcDirTimer +=1;
-                if (npcDirTimer===200) {
-                    npcDirTimer =0;
-                    tempTank.dir = randomDir();
-                    // console.log(tempTank.dir);
+            if (i === 0) {
+                if (tankList[0].type != "P1") {
+                    mode0StageStop -= 1;
+                    if (mode0StageStop === 0) {
+                        clearInterval(setInt);
+                    }
                 }
+            }
+            var tempTank = tankList[i];
+            //随机选择NPC的方向
+            if (tempTank.type === "NPC") {
+                npcDirTimer += 1;
+                if (npcDirTimer === 200) {
+                    npcDirTimer = 0;
+                    tempTank.dir = randomDir();
+                }
+                tempTank.fire();
             }
             if (tempTank.boolKeyDown) {
                 ctx.clearRect(tempTank.oldX, tempTank.oldY, 32, 32);
@@ -172,6 +222,7 @@ var crazyKill = function(stage) {
     }
 };
 var npcDirTimer = 0;
+
 function randomDir() {
     var num = Math.floor((Math.random() * 100) + 1);
     var dir;
@@ -187,30 +238,44 @@ function randomDir() {
     return dir;
 }
 var npcBornTimer = 0;
-function createNewNPC() {
-    npcBornTimer += 1；
-    if(npcBornTimer === 100) {
-        var tempTank = randomTank();
-        for (var i = 0; i < tankList.length; i++) {
-            ////////////////////////////////////
-            ///判断在此位置上有没有坦克在走；
+
+function createNewNPC(maxNum) {
+    if (tankList.length <= maxNum) {
+        npcBornTimer += 1;
+        if (npcBornTimer === 1000) {
+            npcBornTimer = 0;
+            var tempTank = randomTank();
+            for (var i = 0; i < tankList.length; i++) {
+                ////////////////////////////////////
+                ///判断在此位置上有没有坦克在走；
+                // console.log(tankList.length);
+                if (overlap(tempTank.curX, tempTank.curY, 32, tankList[i].curX, tankList[i].curY, 32)) {
+                    tempTank = null;
+                    break;
+                } else {
+                    tankList.push(tempTank);
+                    break;
+                }
+            }
         }
     }
 }
+
 function randomTank() {
     var num = Math.floor((Math.random() * 100) + 1);
-    var tank;
+    var tempTank;
     if (num <= 25) {
-        tank = new Tank(0, 0, "bottom", "NPC");
+        tempTank = new Tank(0, 0, "bottom", "NPC");
     } else if (num > 25 && num <= 50) {
-        tank = new Tank(0, game.h - 32, "top", "NPC");
+        tempTank = new Tank(0, game.h - 32, "top", "NPC");
     } else if (num > 50 && num <= 75) {
-        tank = new Tank(game.w - 32, game.h - 32, "top", "NPC");
+        tempTank = new Tank(game.w - 32, game.h - 32, "top", "NPC");
     } else {
-        tank = new Tank(game.w - 32, 0, "bottom", "NPC");
+        tempTank = new Tank(game.w - 32, 0, "bottom", "NPC");
     }
-    return tank;
+    return tempTank;
 }
+var tankLicense = 0; //新产生一个坦克就+1
 /**
  * 坦克
  * @return {[type]} [description]
@@ -222,10 +287,13 @@ function Tank(x, y, dir, type) {
     this.curX = x; //当前位置
     this.curY = y;
     this.dir = "top";
-    this.type = type;
+    this.type = type; //坦克的类型 P1, P2 , NPC
     this.boolKeyDown = this.type === "NPC" ? true : false; //判断方向是否按下
-
-    this.speed = type !== "NPC" ? 2 : 1;
+    this.license = tankLicense; //用来识别每一个坦克
+    tankLicense += 1; //每创建一个坦克，license加1
+    this.speed = type === "NPC" ? 0.5 : 1;
+    this.fireInterval = 1000; //发射的间隔
+    this.lastFiredTime = 0; //上一次射击的时间
     this.pics = (function() {
         if (type === "NPC") return tankPicsNPC;
         if (type === "P1") return tankPicsP1;
@@ -233,54 +301,7 @@ function Tank(x, y, dir, type) {
     })();
     this.boolAni = false;
     this.delay = 10;
-    this.moveDist = 2;
-}
-Tank.prototype.fire = function() {
-    // body...
-};
-// Tank.prototype.blocked = function(dir) {
-//     var newX = this.curX;
-//     var newY = this.curY;
-//     if (dir === "left") {
-//         newX += this.moveDist;
-//     } else if (dir === "right") {
-//         newX -= this.moveDist;
-//     } else if (dir === "top") {
-//         newY -= this.moveDist;
-//     } else if (dir === "bottom") {
-//         newY += this.moveDist;
-//     }
-//     for (var i = 0; i < tankList.length; i++) {
-//         var obj = tankList[i];
-//         var meet = overlap(newX, newY, obj.curX, obj.curY);
-//     }
-// };
-
-function beBlocked(x, y) {
-    //检查某个坦克是否将要被阻挡
-    var b = [];
-    for (var i = 0; i < tankList.length; i++) {
-        if (overlap(x, y, tankList[i].curX, tankList[i].curY)) {
-            b.push(true);
-        }
-    }
-    if (b.length >= 2 || x < 0 || y < 0 || x + 32 > game.w || y + 32 > game.h) {
-        return true;
-    }
-    return false;
-}
-
-function overlap(x1, y1, x2, y2) {
-    if (x1 >= x2 && x1 <= x2 + 32 && y1 >= y2 && y1 <= y2 + 32) {
-        return true;
-    } else if (x1 + 32 >= x2 && x1 + 32 <= x2 + 32 && y1 >= y2 && y1 <= y2 + 32) {
-        return true;
-    } else if (x1 >= x2 && x1 <= x2 + 32 && y1 + 32 >= y2 && y1 + 32 <= y2 + 32) {
-        return true;
-    } else if (x1 + 32 >= x2 && x1 + 32 <= x2 + 32 && y1 + 32 >= y2 && y1 + 32 <= y2 + 32) {
-        return true;
-    }
-    return false;
+    this.moveDist = 1;
 }
 Tank.prototype.move = function() {
     var tempX = this.curX;
@@ -301,15 +322,199 @@ Tank.prototype.move = function() {
             } else if (this.dir === "bottom") {
                 tempY += this.moveDist;
             }
-            if (!beBlocked(tempX, tempY)) {
+            if (!beBlocked(this.license, tempX, tempY)) {
                 this.curX = tempX;
                 this.curY = tempY;
+            } else {
+                if (this.type === "NPC") {
+                    for (var i = 0; i < this.dir == dirList.length; i++) {
+                        if (this.dir == dirList[i]) {
+                            if (i == 3) {
+                                this.dir = dirList[0];
+                            } else {
+                                this.dir = dirList[i + 1];
+                            }
+                            break;
+                        }
+                    }
+                }
             }
         } else {
             this.delay += this.speed * 100;
         }
     }
 };
+Tank.prototype.fire = function() {
+    var t = Date.now();
+    if (t - this.lastFiredTime >= this.fireInterval) {
+        this.lastFiredTime = t;
+        var bulletStartLoc = calBulletLocBasedOnTank(this.dir, this.curX, this.curY);
+        bulletList.push(new Bullet(this.license, this.dir, this.type, bulletStartLoc[0], bulletStartLoc[1]));
+    }
+};
+
+function calBulletLocBasedOnTank(dir, tankX, tankY) {
+    var x, y;
+    if (dir === "top") {
+        x = tankX + (tankSize.w - bulletSize.w) / 2;
+        y = tankY - bulletSize.h;
+    } else if (dir === "bottom") {
+        x = tankX + (tankSize.w - bulletSize.w) / 2;
+        y = tankY + tankSize.h;
+    } else if (dir === "right") {
+        x = tankX + tankSize.w;
+        y = tankY + (tankSize.h - bulletSize.h) / 2;
+    } else if (dir === "left") {
+        x = tankX - bulletSize.w;
+        y = tankY + (tankSize.h - bulletSize.h) / 2;
+    }
+    return [x, y];
+}
+
+function Bullet(tankLicense, dir, type, x, y) {
+    this.curX = x;
+    this.curY = y;
+    this.oldX = x;
+    this.oldY = y;
+    this.dir = dir;
+    this.speed = 4;
+    this.flyUnit = 2;
+    this.type = type;
+    this.pic = bulletPics[this.dir];
+    this.tankLicense = tankLicense;
+}
+Bullet.prototype.fly = function() {
+    var tempX = this.curX;
+    var tempY = this.curY;
+    this.oldX = this.curX;
+    this.oldY = this.curY;
+    if (this.dir === "top") {
+        tempY -= this.flyUnit;
+    } else if (this.dir === "bottom") {
+        tempY += this.flyUnit;
+    } else if (this.dir === "left") {
+        tempX -= this.flyUnit;
+    } else if (this.dir === "right") {
+        tempX += this.flyUnit;
+    }
+    this.curX = tempX;
+    this.curY = tempY;
+    ctx.clearRect(this.oldX, this.oldY, bulletSize.w, bulletSize.h);
+    ctx.drawImage(elemMisc, this.pic[0], this.pic[1], bulletSize.w, bulletSize.h, this.curX, this.curY, bulletSize.w, bulletSize.h);
+};
+var bulletSize = {
+    w: 8,
+    h: 8
+};
+var boomSize = {
+    w: 64,
+    h: 64
+};
+var bombList = [];
+
+function killTank() {
+    for (var i = bulletList.length - 1; i >= 0; i--) {
+        var curBullet = bulletList[i];
+        for (var j = tankList.length - 1; j >= 0; j--) {
+            var curTank = tankList[j];
+            if (curBullet.tankLicense !== curTank.license) {
+                if (curBullet.type != curTank.type) {
+                    if (overlap(curBullet.curX, curBullet.curY, 8, curTank.curX, curTank.curY, 32)) {
+                        bombList.push(new Bomb(curTank.curX + tankSize.w / 2, curTank.curY + tankSize.h / 2));
+                        ctx.clearRect(curTank.curX, curTank.curY, 32, 32);
+                        tankList.splice(j, 1);
+                        ctx.clearRect(curBullet.curX, curBullet.curY, bulletSize.w, bulletSize.h);
+                        bulletList.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+function killBullet() {
+    var kill = []; //记录要消除的子弹索引
+    var b1, b2;
+    for (var i = 0; i < bulletList.length; i++) {
+        if (i in kill) continue;
+        b1 = bulletList[i];
+        for (var j = 0; j < bulletList.length; j++) {
+            if (j in kill) continue;
+            b2 = bulletList[j];
+            if (i === j || b1.type === b2.type) continue;
+            if (overlap(b1.curX, b1.curY, 8, b2.curX, b2.curY, 8)) {
+                kill.push(i, j);
+            }
+        }
+    }
+    kill.sort();
+    // if (kill.length >0) console.log(kill);
+    for (var k = kill.length - 1; k >= 0; k--) {
+        ctx.clearRect(bulletList[kill[k]].curX, bulletList[kill[k]].curY, bulletSize.w, bulletSize.h);
+        bulletList.splice(kill[k], 1);
+    }
+}
+
+function Bomb(x, y) {
+    this.times = 4; //存在几个回合
+    this.x = x - boomSize.w / 2;
+    this.y = y - boomSize.h / 2;
+}
+Bomb.prototype.showExplode = function() {
+    ctx.clearRect(this.x, this.y, bombSize.w, bombSize.h);
+    if (this.times === 4) {
+        ctx.drawImage(elemBoom, 0, 0, bombSize.w, bombSize.h, this.x, this.y, bombSize.w, bombSize.h);
+    } else if (this.times === 3) {
+        ctx.drawImage(elemBoom, bombSize.w, 0, bombSize.w, bombSize.h, this.x, this.y, bombSize.w, bombSize.h);
+    } else if (this.times === 2) {
+        ctx.drawImage(elemBoom, bombSize.w * 2, 0, bombSize.w, bombSize.h, this.x, this.y, bombSize.w, bombSize.h);
+    } else if (this.times === 1) {
+        ctx.drawImage(elemBoom, bombSize.w * 3, 0, bombSize.w, bombSize.h, this.x, this.y, bombSize.w, bombSize.h);
+    }
+    this.times -= 1;
+};
+
+function beBlocked(license, x, y) {
+    //检查某个坦克是否将要被阻挡
+    var b = false;
+    for (var i = 0; i < tankList.length; i++) {
+        if (tankList[i].license != license) {
+            if (overlap(x, y, tankList[i].curX, tankList[i].curY)) {
+                b = true;
+                break;
+            }
+        }
+    }
+    if (b || x < 0 || y < 0 || x + 32 > game.w || y + 32 > game.h) {
+        return true;
+    }
+    return false;
+}
+
+function overlap(x1, y1, l1, x2, y2, l2) {
+    if (x1 > x2 && x1 < x2 + l2 && y1 > y2 && y1 < y2 + l2) {
+        return true;
+    } else if (x1 + l1 > x2 && x1 + l1 < x2 + l2 && y1 > y2 && y1 < y2 + l2) {
+        return true;
+    } else if (x1 > x2 && x1 < x2 + l2 && y1 + l1 > y2 && y1 + l1 < y2 + l2) {
+        return true;
+    } else if (x1 + l1 > x2 && x1 + l1 < x2 + l2 && y1 + l1 > y2 && y1 + l1 < y2 + l2) {
+        return true;
+    } else if (x1 === x2 && y1 > y2 && y1 < y2 + l2) {
+        return true;
+    } else if (x1 === x2 && y1 + l1 > y2 && y1 + l1 < y2 + l2) {
+        return true;
+    } else if (y1 === y2 && x1 > x2 && x1 < x2 + l2) {
+        return true;
+    } else if (y1 === y2 && x1 + l1 > x2 && x1 + l1 < x2 + l2) {
+        return true;
+    } else if (x1 === x2 && y1 === y2) {
+        return true;
+    }
+    return false;
+}
+
 
 var pageStart = {
     y: 488
@@ -321,6 +526,7 @@ var pageCurtain = {
 var bindKeys = function() {
     window.onkeydown = function(e) {
         var keynum = window.event ? e.keyCode : e.which;
+        console.log(keynum);
         if (game.status === 0) {
             if (keynum === 13) {
                 pageStart.y = 0;
@@ -351,6 +557,13 @@ var bindKeys = function() {
                     tankList[0].boolKeyDown = true;
                     tankList[0].dir = "right";
                 }
+                if (keynum === 72) {
+                    if (tankList.length > 0) {
+                        if (game.mode === 0) {
+                            tankList[0].fire();
+                        }
+                    }
+                }
                 // tankList[0].move();
             }
         }
@@ -364,7 +577,7 @@ var bindKeys = function() {
                 }
             }
         }
-    }
+    };
 };
 // var startTank; //开始界面选择游戏模式的坦克
 var setInt = window.setInterval(function() {
